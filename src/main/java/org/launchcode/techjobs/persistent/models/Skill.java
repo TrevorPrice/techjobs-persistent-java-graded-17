456977e17754c1ae23967c01bb2153dcbf81de0a
TrevorPrice <tprice1245@gmail.com>
2024-06-15 21:14:12 -0500

    Task 4 completed

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -2,7 +2,11 @@ package org.launchcode.techjobs.persistent.models;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
@@ -10,7 +14,15 @@ public class Skill extends AbstractEntity {
     @Size(max = 500, message = "Description can be up to 500 characters.")
     public String description;
 
+    @ManyToMany(mappedBy = "skills")
+    private final List<Job> jobs = new ArrayList<>();
+
     public Skill() {
+    }
+
+    public Skill(String description) {
+        super();
+        this.description = description;
     }
 
     public @Size(max = 500, message = "Description can be up to 500 characters.") String getDescription() {
@@ -19,5 +31,9 @@ public class Skill extends AbstractEntity {
 
     public void setDescription(@Size(max = 500, message = "Description can be up to 500 characters.") String description) {
         this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
     }
 }
